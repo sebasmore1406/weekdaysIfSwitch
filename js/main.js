ad35c682d5a579043c1f7fbecf0d6bb1df2a44fc
SebasMore <sebasmore1406@gmail.com>
2025-02-25 11:16:59 -0600

    Added basic HTML and JS structure

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let dia = 'jueves';
+
+switch (dia) {
+    case 'viernes':
+        console.log('Buen finde!');
+        break;
+    case 'lunes':
+        console.log('Buen inicio de semana!');
+        break;
+    case 'jueves':
+        console.log('JUEBEBEEEEEEEEEEEEEEES');
+        break;   
+    default:
+        console.log('Buen día.');
+}
